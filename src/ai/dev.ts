41a26bccc26b3c3c5b866760787149ac170a4780
Dyre-bored <erdypanapol@gmail.com>
2025-10-03 23:00:24 +0800

    Create dev.ts

---
+++ src/ai/dev.ts
@@ -0,0 +1,7 @@
+// Flows will be imported for their side effects in this file.
+import './flows/generate-drug-game-flow';
+import './flows/generate-flashcards-flow';
+import './flows/generate-med-term-flow';
+import './flows/generate-quiz-questions-flow';
+import './flows/generate-vital-sign-flow';
+import './flows/update-leaderboard-flow';
